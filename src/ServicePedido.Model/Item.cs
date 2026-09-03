7247ee61b72c7371e9a783c0d09b2b4b309e0e85
Alessandro Tulioli <desmecad@hotmail.com>
2026-09-03 17:16:13 -0300

    Criação da estrutura dos projeto e modelos

---
+++ src/ServicePedido.Model/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServicePedido.Model
+{
+    public class Item
+    {
+        public required string NomeProduto { get; set; }
+        public required int Quantidade { get; set; }
+        public required decimal PrecoUnitario { get; set; }
+    }
+}
